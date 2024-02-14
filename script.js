7f1d017509ce34eb44a69ae3bbafcbb781afb30e
Abhi <abhikappana@gmail.com>
2024-02-14 15:37:55 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+const alternatives = [
+    {text:"", image:"images/cat-01.gif"},
+    {text:"I promise it will be unforgettable", image:"images/cat-02.gif"},
+    {text:"Think again", image:"images/cat-03.gif"},
+    {text:"Come on, dare to say yes", image:"images/cat-04.gif"},
+    {text:"Don't let fear stop you", image:"images/cat-05.gif"},
+]
+const ohyes = {text:"I knew you would accept", image:"images/cat-yes.gif"}
+const cat = document.querySelector('.cat')
+const text = document.querySelector('.text')
+const buttons = document.querySelectorAll('.button')
+const errorButton = document.querySelector('.button__error')
+
+let count = 0;
+
+function updateDisplay(item){
+    cat.src = item.image
+    text.innerHTML = item.text
+}
+
+errorButton.addEventListener('click', ()=>{
+    count = 0;
+    updateDisplay(alternatives[count])
+    buttons.forEach(btn => btn.style.display = 'inline-block')
+    errorButton.style.display = 'none'
+})
+
+buttons.forEach(button => {
+    button.addEventListener('click', ()=>{
+        if(button.textContent == "Yes"){
+            updateDisplay(ohyes)
+            buttons.forEach(btn => btn.style.display = 'none')
+        }
+        if(button.textContent == 'No'){
+            count++
+            if(count < alternatives.length){
+                updateDisplay(alternatives[count])
+            }else{
+                buttons.forEach(btn => btn.style.display = 'none')
+                errorButton.style.display = 'inline-block'
+            }
+        }
+    })
+})
